13efaf81308ae0ae158d6f369cd90ef0f667ca44
rfields4 <rfields4@ycp.edu>
2023-03-22 19:33:04 -0400

    Created untested QueenModel

---
+++ StumpMasters/src/pieceModels/QueenModel.java
@@ -0,0 +1,22 @@
+package pieceModels;
+
+import java.util.Collection;
+import java.util.List;
+
+public class QueenModel extends PieceModel{
+	
+	public QueenModel() {
+		super();
+		
+	}
+	
+	public QueenModel(int xpos, int ypos, int color){
+		super(xpos,ypos,color);
+	}
+	
+	public List<Integer[]> getValidMoves(PieceModel[][] board){
+		List<Integer[]> possibleMoves = super.getDiagonalMoves(board);
+		possibleMoves.addAll((Collection<Integer[]>)(super.getDiagonalMoves(board)));
+		return possibleMoves;
+	}
+}
