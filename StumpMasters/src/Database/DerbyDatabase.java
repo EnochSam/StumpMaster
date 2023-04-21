c37228633b719ab3b9e5f63f8843a28d73c5a677
RomanStump <stump6roman@gmail.com>
2023-04-20 18:36:36 -0700

    set board function works with derby database

--- StumpMasters/src/Database/DerbyDatabase.java
+++ StumpMasters/src/Database/DerbyDatabase.java
@@ -9,6 +9,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import models.Game;
 import models.Player;
 import pieceModels.Piece;
 
@@ -27,7 +28,11 @@ public class DerbyDatabase implements IDatabase {
 
 	private static final int MAX_ATTEMPTS = 10;
 
+	Game model;
 	
+	public void setModel(Game model) {
+		this.model = model;
+	}
 	
 	public<ResultType> ResultType executeTransaction(Transaction<ResultType> txn) {
 		try {
@@ -99,7 +104,7 @@ public class DerbyDatabase implements IDatabase {
 							")"
 					);
 					stmt1.executeUpdate();
-				
+					
 					stmt2 = conn.prepareStatement(
 						"create table pieces (" +
 						"	piece_id integer primary key " +
@@ -179,12 +184,19 @@ public class DerbyDatabase implements IDatabase {
 	public static void main(String[] args) throws IOException {
 		//System.out.println("Creating tables...");
 		DerbyDatabase db = new DerbyDatabase();
-		db.createTables();
+		Game model = new Game();
+		db.setModel(model);
+		db.setBoard("");
 		
-		System.out.println("Loading initial data...");
-		db.loadInitialData();
+		
+		//DerbyDatabase db = new DerbyDatabase();
+		//db.createTables();
+		
+		//System.out.println("Loading initial data...");
+		//db.loadInitialData();
 		
 		System.out.println("Success!");
+		//*/
 	}
 	
 	@Override
@@ -195,10 +207,94 @@ public class DerbyDatabase implements IDatabase {
 
 	@Override
 	public void setBoard(String boardLocations) {
-		// TODO Auto-generated method stub
 		
-	}
+		if(boardLocations.length() == 0) {
+		executeTransaction(new Transaction<Boolean>() {
+			@Override
+			public Boolean execute(Connection conn) throws SQLException {
+				PreparedStatement stmt1 = null;
+				PreparedStatement stmt2 = null;
+				ResultSet resultSet = null;		
+				DerbyDatabase db = new DerbyDatabase();				
 
+				//if gameMoves.length is = 0, drop tables
+				if(boardLocations.length() ==  0) {
+					try {
+						stmt1 = conn.prepareStatement(
+								"drop table pieces"
+						);
+						stmt1.executeUpdate();
+						System.out.println("Statement 1 Executed");
+						stmt2 = conn.prepareStatement(
+								"drop table players"
+						);
+						stmt2.executeUpdate();
+						System.out.println("Statement 2 Executed");
+						
+						DBUtil.closeQuietly(stmt1);
+						DBUtil.closeQuietly(stmt2);
+						
+						
+					
+						return true;
+					}
+					finally {
+						DBUtil.closeQuietly(stmt1);
+						DBUtil.closeQuietly(stmt2);
+					}
+				}
+				return false;
+			}
+				
+		});	
+		
+		//create tables by create table function in this file
+		createTables();
+		System.out.println("Tables Created");
+		loadInitialData();
+		}
+		executeTransaction(new Transaction<Boolean>() {
+			Piece[][] board = new Piece[8][8];
+			public Boolean execute(Connection conn) throws SQLException {
+				PreparedStatement stmt1 = null;
+				PreparedStatement stmt2 = null;
+				ResultSet resultSet = null;					
+				try {
+			
+					//create pieces for all tuples where isCaptured = false, add piece to board at right location
+					stmt1 = conn.prepareStatement(
+							"select type, x, y, color "+
+							"from pieces "+
+							"where isCaptured  = 'false'"
+							);
+					resultSet = stmt1.executeQuery();
+			
+				
+				
+					while(resultSet.next()) {
+						String type = resultSet.getObject(1).toString();
+						int x = Integer.parseInt(resultSet.getObject(2).toString());
+						int y = Integer.parseInt(resultSet.getObject(3).toString());
+						int color = Integer.parseInt(resultSet.getObject(4).toString());
+						Piece piece = Piece.findPiece(type);
+						piece.setXpos(x);
+						piece.setYpos(y);
+						piece.setColor(color);
+						board[y][x] = piece;
+						
+						
+						//set board in model			
+					}
+					model.setBoard(board);
+					return true;
+			}
+		finally {
+			DBUtil.closeQuietly(stmt1);
+			}
+		}	
+	});
+	}
+		
 	@Override
 	public boolean moveValidMove(String newPieceLoc, String attemptingToMove, String player) {
 		// TODO Auto-generated method stub
--- StumpMasters/src/Database/IDatabase.java
+++ StumpMasters/src/Database/IDatabase.java
@@ -1,8 +1,10 @@
 package Database;
 
+import java.sql.SQLException;
+
 public interface IDatabase {
 	public String getPossibleMoves(String clickedOnLocation, String playerTurn);
-	public void setBoard(String boardLocations);
+	public void setBoard(String boardLocations) throws SQLException;
 	public boolean moveValidMove(String newPieceLoc, String attemptingToMove,String player);
 	
 }
